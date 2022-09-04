cd6214ca70528f9867d2f6a97bd912abf38c65d9
Yingyang Zhang <yingyang.zhang95@gmail.com>
2022-09-04 09:25:40 -0400

    commit tree 184798a200907aa39524cac07e4f7554abc118ef

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+const record = [
+    {year: "2015", result: "W"},
+    {year: "2014", result: "N/A"},
+    {year: "2013", result: "L"},
+]
+
+function superbowlWin(array){
+    let win = array.find(isWin);
+    
+    if(win !== undefined){
+        return win.year
+    }else{
+        return;
+    };
+}
+
+function isWin(item){
+    return (item.result === "W");
+}
